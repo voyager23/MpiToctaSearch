5fb3f76508240531b0c03311d7376e02294aee3d
Mike <nobody@nowhere>
2018-12-12 18:27:08 +0100

    Double count and doubles transmitted to work nodes.

--- mpi_src/mpi_tocta_search.c
+++ mpi_src/mpi_tocta_search.c
@@ -28,6 +28,10 @@
 
 int main(int argc, char* argv[])
 {
+	#define TAG_NDOUBLES 2
+	#define TAG_DDATA 4
+	#define ROOT 0
+	
     int rank, size, len;
     char version[MPI_MAX_LIBRARY_VERSION_STRING];
     char pname[MPI_MAX_PROCESSOR_NAME];
@@ -35,49 +39,53 @@ int main(int argc, char* argv[])
     gsl_complex target;
     int quiet = 0;
     int list  = 0;
+ 
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Get_library_version(version, &len);
     MPI_Get_processor_name(pname, &len);
-    if (rank != 0) {
-		
+    
+    if (rank != 0) {		
 		// Work Process
-		// printf("Hello world - I am %d of %d \t(%s)\n",rank, size, pname);
-		// fflush(stdout);
+		
+		int result_code = 0;
 		MPI_Status status;
-		int double_count = 4992 * 2;
-		//MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
-		//MPI_Get_count(&status, MPI_DOUBLE, &double_count);
-		double* double_buffer = malloc(sizeof(MPI_DOUBLE)*double_count);
-		MPI_Bcast(double_buffer, double_count, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+		int double_count = 0;
+		gsl_vector_complex* compact = NULL;
 		
-		printf("Process %d received %d doubles.\n", rank, double_count);
-		fflush(stdout);	
+		result_code = MPI_Bcast(&double_count, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+		if(result_code != MPI_SUCCESS) printf("\nWork process %d reports Bcast error.\n", rank);		
+		printf("Process %d received %d as double_count.\n", rank, double_count); fflush(stdout);
+		compact = gsl_vector_complex_calloc(double_count / 2);
+		MPI_Bcast(compact->data, double_count, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+		printf("BCast received.\n");
 		
-	} else {			
-		
+	} else {		
 		// Root Process
+		
 		int result_code;
 		gsl_vector_complex *compact = NULL;
+		int nDoubles = 0;
 		
-		printf("Hello, world, I am Root of %d \t(%s) ",size, pname);
+		// printf("Hello, world, I am Root of %d \t(%s) ",size, pname);
 		get_options(argc, argv, &target, &quiet, &list);
 		PRT_COMPLEX(target); NL;
-		printf("Quiet: %d	List: %d \n", quiet, list);		
 		fflush(stdout);
 		// Prepare a contiguous vector of complex numbers (compact) using equalsums_database.bin
 		// Each group of 4 represents an equalsum of target value.
 		// ??Create a checksum??
 		
-		compact_equalsums("../data/equalsums_database.bin", &compact, &target);		
-		
-		// Broadcast/Send vector to work processes.
-		result_code = MPI_Bcast(compact->data, (compact->size)*2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		
-		// ??Receive/confirm checksum??
-		
+		compact_equalsums("../data/equalsums_database.bin", &compact, &target);
+		nDoubles = (compact->size) * 2;			
+		// Broadcast number of doubles in main message
+		if(MPI_SUCCESS != MPI_Bcast(&nDoubles, 1, MPI_INT, ROOT, MPI_COMM_WORLD)) 
+			printf("\nWork process %d reports Bcast error.\n", rank);
+		// Broadcast the doubles
+		MPI_Bcast(compact->data, nDoubles, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+		printf("Bcast completed.\n");
+						
 	}
 	
     MPI_Finalize();
