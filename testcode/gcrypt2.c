c8969eea2b35e8ebb63e59e887b7c0d8f4ffbbc1
mike <mike@nowhere>
2019-01-24 11:39:55 +0100

    Working in testcode. Added compare and qsort to gcrypt2.c

--- testcode/gcrypt2.c
+++ testcode/gcrypt2.c
@@ -29,9 +29,9 @@
 #include <gsl/gsl_complex.h>
 #include <gsl/gsl_complex_math.h>
 #include <gsl/gsl_matrix.h>
+#include "../mpi_include/toolbox.h"
 
-
-
+//----------------------------------------------------------------------
 double soln0[4][8] = {	{4,11, 1,6, 2,13, 2,3},
 						{2,13, 3,8, 1,2, 3,10},
 						{1,2, 4,9, 2,7, 2,15},
@@ -41,7 +41,7 @@ double soln1[4][8] = {	{2,5, 4,11, 1,10, 2,7},
 						{1,10, 1,6, 3,8, 4,9},
 						{3,8, 2,13, 3,10, 1,2},
 						{3,10, 2,3, 2,5, 2,15} };
-						
+//----------------------------------------------------------------------			
 void prt_gsl_matrix_complex(gsl_matrix_complex *m);
 void prt_gsl_matrix_complex(gsl_matrix_complex *m) {
 	for(int row = 0; row < 4; ++row) {
@@ -53,7 +53,7 @@ void prt_gsl_matrix_complex(gsl_matrix_complex *m) {
 	}
 	printf("\n");
 }
-
+//----------------------------------------------------------------------
 char* posn_independant_signature(gsl_matrix_complex *m);
 char* posn_independant_signature(gsl_matrix_complex *m) {
 	/* return a pointer to a 20 byte sha128 digest
@@ -67,7 +67,7 @@ char* posn_independant_signature(gsl_matrix_complex *m) {
 
 	
 }
-
+//======================================================================
 int main(int argc, char **argv)
 {
 	gsl_matrix_complex *mat0 = gsl_matrix_complex_alloc(4,4);
@@ -90,6 +90,14 @@ int main(int argc, char **argv)
 	prt_gsl_matrix_complex(mat0);
 	prt_gsl_matrix_complex(mat1);
 	
+	qsort(gsl_matrix_complex_ptr(mat0, 0, 0), 4, sizeof(gsl_complex), compare_gsl_complex);
+	qsort(gsl_matrix_complex_ptr(mat0, 1, 0), 4, sizeof(gsl_complex), compare_gsl_complex);
+	qsort(gsl_matrix_complex_ptr(mat0, 2, 0), 4, sizeof(gsl_complex), compare_gsl_complex);
+	qsort(gsl_matrix_complex_ptr(mat0, 3, 0), 4, sizeof(gsl_complex), compare_gsl_complex);
+	
+	prt_gsl_matrix_complex(mat0);
+	prt_gsl_matrix_complex(mat1);
+		
 	
 	
 	return 0;
