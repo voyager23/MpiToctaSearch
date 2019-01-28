bc347b1471862353ebc2ef430db1e7565a65fd93
Mike <XXX@YYYY.ZZZ>
2019-01-28 18:48:39 +0100

    Working code restore point.

--- testcode/gcrypt2.c
+++ testcode/gcrypt2.c
@@ -40,9 +40,21 @@ double soln0[4][8] = {	{4,11, 1,6, 2,13, 2,3},
 						
 double soln1[4][8] = {	{2,5, 4,11, 1,10, 2,7},
 						{1,10, 1,6, 3,8, 4,9},						
-						{3,8, 2,13, 3,10, 1,2}, 	// original data
-						//{3,8, 2,13, 3,10, 2,1},	// debug data
+						{3,8, 2,13, 3,10, 1,2},
 						{3,10, 2,3, 2,5, 2,15} };
+						
+						
+double soln2[4][8] = {	{2,3, 2,13, 1,6, 4,11},
+						{1,6, 3,8, 4,9, 1,10},						
+						{4,9, 1,2, 2,15, 2,7},
+						{2,15, 3,10, 2,3, 2,5} };
+						
+						
+double soln3[4][8] = {	{2,3, 3,10, 2,15, 2,5},
+						{2,15, 1,2, 4,9, 2,7},						
+						{4,9, 3,8, 1,6, 1,10},
+						{1,6, 2,13, 2,3, 4,11} };
+						
 //----------------------------------------------------------------------			
 void prt_gsl_matrix_complex(gsl_matrix_complex *m);
 void prt_gsl_matrix_complex(gsl_matrix_complex *m) {
@@ -119,13 +131,13 @@ void posn_independant_signature(gsl_matrix_complex *m, char *digest) {
 int main(int argc, char **argv)
 {
 
-	gsl_matrix_complex *mat0 = gsl_matrix_complex_alloc(4,4);
+	gsl_matrix_complex *mat2 = gsl_matrix_complex_alloc(4,4);
 	gsl_matrix_complex *mat1 = gsl_matrix_complex_alloc(4,4);
 	
 	for(int row = 0; row < 4; ++row) {
 		for(int col = 0; col < 8; col+=2) {
 			gsl_complex t = gsl_complex_rect(soln0[row][col], soln0[row][col+1]);
-			gsl_matrix_complex_set(mat0, row, col/2, t);
+			gsl_matrix_complex_set(mat2, row, col/2, t);
 		}
 	}
 	
@@ -136,12 +148,12 @@ int main(int argc, char **argv)
 		}
 	}
 	
-	prt_gsl_matrix_complex(mat0);
+	prt_gsl_matrix_complex(mat2);
 	prt_gsl_matrix_complex(mat1);
 		
 	char digest[20];
 	 
-	posn_independant_signature(mat0, digest);
+	posn_independant_signature(mat2, digest);
 	printf("Signature.\n");
 	for(int i = 0; i < 20; ++i) printf("%02x ", digest[i]&0x00ff);
 	printf("\n");
