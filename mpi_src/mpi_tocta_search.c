ba92ce99c6a7982a11a1102e3056c7580086efe0
Mike <XXX@YYYY.ZZZ>
2019-02-11 17:20:30 +0100

    Restore point for mpi_tocta_search.c Changing the code which prints out unique digests.

--- mpi_src/mpi_tocta_search.c
+++ mpi_src/mpi_tocta_search.c
@@ -403,9 +403,8 @@ int main(int argc, char* argv[])
 		
 		// free buffer
 		free(buffer);
-//===================
+
 		char current_digest[dlen];
-		// qsort digest_ptrs
 		printf("Digest_ptrs signatures.\n");
 		int signature_count = 0;
 		for(int j = 0; j < digest_ptrs->size; ++j) {
@@ -415,26 +414,12 @@ int main(int argc, char* argv[])
 				printf("\n");
 				memcpy(current_digest, d, dlen);
 				++signature_count;
-				// Write a copy of sdata to file
+				// Write a copy of soln_data to file
+				// Write signature
 			}
 		}
 		printf("Signature count: %d\n", signature_count);
 		printf("End Digest_ptrs signatures.\n");
-//===================
-#if(0)			
-		int signature_count = 0;
-		NL;
-		for(int j = 0; j < digest_ptrs->size; ++j) {
-			Solution_Data *sdp = (Solution_Data*)gsl_vector_ulong_get(digest_ptrs,j );
-			if(1) {				
-				for(int i = 0; i < dlen; ++i) printf("%02x ", (*(sdp->pisig + i)) & 0x00ff);
-				printf("\n");		
-				
-			}
-			signature_count+=1;
-		}
-#endif
-//=====================	
 		printf("Signature print count: %d\n", signature_count);
 
 		// Cleanup code
